75cd686421654e1a8301cf72c624a4593c0393e8
Kakaede <maulanasulami271@gmail.com>
2023-10-05 23:26:30 +0700

    Added portfolio section and polished about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,23 +6,16 @@ const About = () => {
 
       <div className="p-4 flex flex-col justify-center w-full h-full text-center">
         <div className="pb-6">
-          <p className="font-rubik-mono-one text-3xl sm:text-4xl font-bold  hover:duration-300  inline border-b-2">
+          <p className="font-rubik-mono-one text-3xl sm:text-4xl inline border-b-2">
             About
           </p>
         </div>
 
         <p className="font-momomaniac-one text-1xl sm:text-xl text-gray-300 py-4 ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, reprehenderit laborum voluptate laudantium quos ullam aperiam accusantium architecto facilis eum. Sit ex totam tempora consequuntur minima esse fugiat unde adipisci asperiores a ipsa amet exercitationem suscipit distinctio officiis, nisi aliquam ducimus soluta? Tempore officia illum inventore recusandae sit repudiandae provident obcaecati ex delectus. Sit, laudantium nemo consequuntur unde ipsa distinctio fugiat incidunt, amet ea nihil dolorem harum eius at corporis accusamus excepturi magnam, voluptatem corrupti? Consectetur laboriosam repellat nobis dolore repudiandae unde possimus? Itaque esse animi quaerat ab, eos sed iste est numquam voluptatibus nobis consequatur, temporibus perspiciatis dolor id?
-
-
-          IS IT GREENNNNNN???
         </p>
 
         <br />
-
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis neque ex, doloribus sint minima at id laborum voluptatum ea dolorem delectus reprehenderit repellat quaerat quisquam dolores! Corporis est alias eius ut atque dolore, iusto voluptatibus tenetur magni laborum dolores molestias beatae consectetur, laboriosam quae assumenda fugit, dolor officia inventore id?
-        </p>
       </div>
     </div>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,14 +1,17 @@
 const Portfolio = () => {
+  return (
+    <div
+      name="portfolio"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 text-white"
+    >
 
-    return <div>
-        <div>
-            <div>
-                <p className="text-2xl">
-                    Portfolio
-                </p>
-            </div>
+      <div className="p-4 flex flex-col justify-center w-full h-full text-center">
+        <div className="pb-6">
+          <p className="font-rubik-mono-one text-3xl sm:text-4xl inline border-b-2">My Works</p>
         </div>
+      </div>
     </div>
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
