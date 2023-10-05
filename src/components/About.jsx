a16566aaf5ed98e6e23f2dd237038a3ec7ed4b63
Kakaede <maulanasulami271@gmail.com>
2023-10-05 22:38:32 +0700

    TEst push

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,6 +13,9 @@ const About = () => {
 
         <p className="font-momomaniac-one text-1xl sm:text-xl text-gray-300 py-4 ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, reprehenderit laborum voluptate laudantium quos ullam aperiam accusantium architecto facilis eum. Sit ex totam tempora consequuntur minima esse fugiat unde adipisci asperiores a ipsa amet exercitationem suscipit distinctio officiis, nisi aliquam ducimus soluta? Tempore officia illum inventore recusandae sit repudiandae provident obcaecati ex delectus. Sit, laudantium nemo consequuntur unde ipsa distinctio fugiat incidunt, amet ea nihil dolorem harum eius at corporis accusamus excepturi magnam, voluptatem corrupti? Consectetur laboriosam repellat nobis dolore repudiandae unde possimus? Itaque esse animi quaerat ab, eos sed iste est numquam voluptatibus nobis consequatur, temporibus perspiciatis dolor id?
+
+
+          TEST UPDATE
         </p>
 
         <br />
