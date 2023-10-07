5c8d2f0c41b7d93add6fdbbbddf37187b6c86382
Kakaede <maulanasulami271@gmail.com>
2023-10-08 00:23:20 +0700

    Added contacts for web and mobile

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const Contacts =
+  [
+    {
+      id: 1,
+      link: "Email",
+      icon: faEnvelope,
+      url: "",
+    },
+    {
+      id: 2,
+      link: "LinkedIn",
+      icon: faLinkedin,
+      url: "",
+    },
+    {
+      id: 3,
+      link: "Github",
+      icon: faGithub,
+      url: "https://github.com/MaulanaAhmadSulami",
+    },
+  ];
+
+
+export default Contacts;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,35 +3,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin} from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Contacts from "./Contacts";
 
 const Footer = () => {
-    const contactLinks = [
-      {
-        id: 1,
-        link: "Email",
-        icon: faEnvelope,
-        url: "",
-      },
-      {
-        id: 2,
-        link: "LinkedIn",
-        icon: faLinkedin,
-        url: "",
-      },
-      {
-        id: 3,
-        link: "GitHub",
-        icon: faGithub,
-        url: "https://github.com/MaulanaAhmadSulami",
-      },
-    ];
-  
     return (
       <footer className="bg-neutral-900 p-4">
         <div className="container mx-auto flex justify-center items-center">
             {/* mobile */}
             <ul className="md:hidden flex flex-col space-y-2">
-            {contactLinks.map(({ id, link, icon, url }) => (
+            {Contacts.map(({ id, link, icon, url }) => (
               <li key={id}>
                 <a
                   href={url}
@@ -47,7 +27,7 @@ const Footer = () => {
           </ul>
 
           <ul className="hidden md:flex space-x-6">
-            {contactLinks.map(({ id, link, icon, url }) => (
+            {Contacts.map(({ id, link, icon, url }) => (
               <li key={id}>
                 <a
                   href={url}
